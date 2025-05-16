1230345e0358e085e3bd88f78df869ac5985ac4f
SCWinter259 <catungthongkhi2002@gmail.com>
2025-05-15 19:54:26 -0600

    added basic CRUD ops, 9

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -1,6 +1,8 @@
 using api.Data;
+using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers;
 
@@ -16,17 +18,18 @@ public class StockController : ControllerBase
     }
 
     [HttpGet]
-    public IActionResult GetAll()
+    public async Task<IActionResult> GetAll()
     {
-        var stocks = _context.Stocks.ToList().Select(s => s.ToStockDto());
+        var stocks = await _context.Stocks.ToListAsync();
+        var stockDto = stocks.Select(s => s.ToStockDto());
 
         return Ok(stocks);
     }
 
     [HttpGet("{id}")]
-    public IActionResult GetById([FromRoute] int id)
+    public async Task<IActionResult> GetById([FromRoute] int id)
     {
-        var stock = _context.Stocks.Find(id);
+        var stock = await _context.Stocks.FindAsync(id);
 
         if (stock == null)
         {
@@ -34,5 +37,64 @@ public class StockController : ControllerBase
         }
         
         return Ok(stock.ToStockDto());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
+    {
+        var stockModel = stockDto.ToStockFromCreateDto();
+        await _context.Stocks.AddAsync(stockModel);
+        await _context.SaveChangesAsync();
+        
+        /*
+         * this return is essential for RESTful practices because:
+         * - it returns the correct HTTP status code
+         * - it provides the url of the newly created resource
+         * - it returns the representation of the created resource
+         */
+        return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+    {
+        var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+
+        // check if the id is valid or not
+        if (stockModel == null)
+        {
+            return NotFound();
+        }
+        
+        stockModel.Symbol = updateDto.Symbol;
+        stockModel.CompanyName = updateDto.CompanyName;
+        stockModel.Purchase = updateDto.Purchase;
+        stockModel.LastDiv = updateDto.LastDiv;
+        stockModel.Industry = updateDto.Industry;
+        stockModel.MarketCap = updateDto.MarketCap;
+
+        await _context.SaveChangesAsync();
+        
+        return Ok(stockModel.ToStockDto());
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+
+        // check if the id is valid or not
+        if (stockModel == null)
+        {
+            return NotFound();
+        }
+        
+        _context.Stocks.Remove(stockModel);
+        await _context.SaveChangesAsync();
+        
+        // success return for deleting
+        return NoContent();
     }
 }
